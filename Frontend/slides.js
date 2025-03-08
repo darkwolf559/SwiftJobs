4ded367300cbad992dc8227fe48f167578b8ffaa
Pasan24 <pramudithapasan076@gmail.com>
2025-03-08 13:51:22 +0530

    Data For the  Onboarding Screen

---
+++ Frontend/slides.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: 'Welcome to SwiftJob',
+    description: 'Discover your dream job with ease. Explore thousands of job opportunities tailored just for you.',
+    image: require('./src/assets/swiftjob.jpg'),
+  },
+  {
+    id: 2,
+    title: 'Personalized Recommendations',
+    description: 'Tell us about your skills and preferences, and we’ll provide job recommendations that suit your profile.',
+    image: require('./src/assets/personalize.jpg'),
+  },
+  {
+    id: 3,
+    title: 'Connect with Employers',
+    description: 'Apply directly to jobs and connect with top employers to grow your career.',
+    image: require('./src/assets/employee.jpg'),
+  },
+  {
+    id: 4,
+    title: 'Stay Updated',
+    description: 'Get real-time notifications for new job openings and application updates to never miss an opportunity.',
+    image: require('./src/assets/notification.jpg'),
+  },
+];

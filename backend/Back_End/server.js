3fd594529bcbfb2105f1f3a361c102068b20f81f
Chenath <chenathk@gmail.com>
2025-03-08 13:14:14 +0530

    project evirement

---
+++ backend/Back_End/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import signupRoutes from "./routes/signup.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/signup", signupRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

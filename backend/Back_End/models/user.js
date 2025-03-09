4f3be38a341478f8c286173a62e6fe38ffe2c300
Chenath <chenathk@gmail.com>
2025-03-09 12:26:42 +0530

    Update user.js
    Added mobile number as the unique

--- backend/Back_End/models/user.js
+++ backend/Back_End/models/user.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     username: { type: String, required: true, unique: true },
-    mobileNumber: { type: String, required: true },
+    mobileNumber: { type: String, required: true, unique: true },
 }, { timestamps: true });
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", userSchema);
 export default User;

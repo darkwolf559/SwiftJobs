b932833c24921c0ff543da8342ed8cda9296a59b
Chenath <chenathk@gmail.com>
2025-03-11 11:44:12 +0530

    Updated user schema
    added more requirements to update the user profile

--- backend/Back_End/models/user.js
+++ backend/Back_End/models/user.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     fullName: { type: String, required: true },
+    dateOfBirth: { type: String, required: true },
+    gender: { type: String, required: true },
+    homeAddress: { type: String, required: true },
+    country: { type: String, required: true },
+    zipCode: { type: String, required: true },
+    college: { type: String, required: true },
+    degree: { type: String, required: true },
+    higherSecondaryEducation: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    username: { type: String, required: true, unique: true },
-    mobileNumber: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
 }, { timestamps: true });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", UserSchema);
 export default User;

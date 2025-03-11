d09692d560e377067ecd830b3571b34eec97017c
Chenath <chenathk@gmail.com>
2025-03-11 11:47:44 +0530

    Update server.js
    Added new imports for the user profile

--- backend/Back_End/server.js
+++ backend/Back_End/server.js
@@ -1,18 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
-import dbConnect from "./config/dbconnect.js";
-import signupRoutes from "./routes/signup.js";
+import connectDB from "./config/dbConnect.js";
 import loginRoutes from "./routes/login.js";
+import signupRoutes from "./routes/signup.js";
+import profileRoutes from "./routes/profile.js";
 
 dotenv.config();
+connectDB();
+
 const app = express();
-
-dbConnect();
-
 app.use(express.json());
 
-app.use("/api/signup", signupRoutes);
 app.use("/api/login", loginRoutes);
+app.use("/api/signup", signupRoutes);
+app.use("/api/profile", profileRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

c74440b2c082f2fd04dc5731992f78277a369716
Chenath <chenathk@gmail.com>
2025-03-16 20:51:26 +0530

    Update job.js
    adjusted the user job schema

--- backend/Back_End/models/job.js
+++ backend/Back_End/models/job.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
-const jobSchema = new mongoose.Schema(
-  {
+const jobSchema = new mongoose.Schema({
     jobTitle: { type: String, required: true },
     jobDescription: { type: String, required: true },
     payment: { type: Number, required: true },
@@ -12,11 +11,8 @@ const jobSchema = new mongoose.Schema(
     workingHoursPerDay: { type: Number, required: true },
     employerMobile: { type: String, required: true },
     applicationDeadline: { type: Date, required: true },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  },
-  { timestamps: true }
-);
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Employer's ID
+}, { timestamps: true });
 
 const Job = mongoose.model("Job", jobSchema);
-
 export default Job;
